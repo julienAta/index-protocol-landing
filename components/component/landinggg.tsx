71d66db45ba0bf0a222a4d436a26e847f9d36d2d
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-13 00:56:08 +0100

    v0

--- components/component/landinggg.tsx
+++ components/component/landinggg.tsx
@@ -112,7 +112,7 @@ export function Landinggg() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        {/* <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container space-y-12 px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
@@ -463,21 +463,21 @@ export function Landinggg() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      {/* <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          © Acme Inc. All rights reserved.
+          © Saga Monkes DAO. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Terms of Service
+         
           </Link>
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Privacy
           </Link>
         </nav>
-      </footer>
+      </footer> */}
     </div>
   );
 }
