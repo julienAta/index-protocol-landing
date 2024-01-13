ad9bb69a8b078b1017688953e1be3359cac0922e
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-13 18:41:05 +0100

    cleanup

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,7 +1,3 @@
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import SparklesCore from "./sparkles";
 import { Landinggg } from "./component/landinggg";
 
 export default function Landing() {
