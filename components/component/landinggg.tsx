3e9e6fe13dfbe31af7496e75741ee0234b8a42fa
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-13 18:51:01 +0100

    footer

--- components/component/landinggg.tsx
+++ components/component/landinggg.tsx
@@ -467,19 +467,17 @@ export function Landinggg() {
           </div>
         </section> */}
       </main>
-      {/* <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500 dark:text-gray-400">
-          © Saga Monkes DAO. All rights reserved.
-        </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-gray-500 dark:text-gray-400">Made by jjx</p>
+        {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
          
           </Link>
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Privacy
           </Link>
-        </nav>
-      </footer> */}
+        </nav> */}
+      </footer>
     </div>
   );
 }
