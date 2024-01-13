adbecd12160ccc16db4cfdf7be03dceb7c362ca6
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-13 13:14:27 +0100

    title

--- components/component/landinggg.tsx
+++ components/component/landinggg.tsx
@@ -54,7 +54,7 @@ export function Landinggg() {
             <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
               <div>
                 <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                  Saga Monkes DAO
+                  Saga Monkes Community
                 </h1>
               </div>
               <div className="flex flex-col items-start space-y-4">
