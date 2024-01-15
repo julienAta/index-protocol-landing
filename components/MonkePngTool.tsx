ea4b864fd8dbc441821ea60a5220372a9e092e19
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-15 19:59:54 +0100

    custom background feature

--- components/MonkePngTool.tsx
+++ components/MonkePngTool.tsx
@@ -1,21 +1,76 @@
 "use client";
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useEffect, useRef } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import Image from "next/image";
 
 const MonkePngTool: React.FC = () => {
   const [number, setNumber] = useState<string>("");
+  const [backgroundImageFile, setBackgroundImageFile] = useState<File | null>(
+    null
+  );
+  const [backgroundImageSrc, setBackgroundImageSrc] = useState<string>("");
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const baseUrl =
     "https://shdw-drive.genesysgo.net/HCA6JCoGqbRnyoaZE4fg5ejzDq64jR7N6eH5MuTUd2pw/";
+  const imageUrl = `${baseUrl}${number}.png`;
 
-  // Function to handle input change with TypeScript type for the event
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNumber(e.target.value);
   };
 
-  // Construct the image URL
-  const imageUrl = `${baseUrl}${number}.png`;
+  const handleBackgroundImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setBackgroundImageFile(e.target.files[0]);
+      const fileReader = new FileReader();
+      fileReader.onload = (event) => {
+        if (!event.target) return;
+        setBackgroundImageSrc(event.target.result as string);
+      };
+      fileReader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+
+    // Clear the canvas before drawing new images
+    ctx?.clearRect(0, 0, 500, 500);
+
+    const drawMonkeImage = () => {
+      if (number) {
+        const monkeImg = new Image();
+        monkeImg.src = imageUrl;
+        monkeImg.onload = () => {
+          ctx?.drawImage(monkeImg, 0, 0, 500, 500);
+        };
+      }
+    };
+
+    if (ctx && backgroundImageSrc) {
+      const backgroundImg = new Image();
+      backgroundImg.onload = () => {
+        ctx.drawImage(backgroundImg, 0, 0, 500, 500);
+        drawMonkeImage();
+      };
+      backgroundImg.src = backgroundImageSrc;
+    } else {
+      drawMonkeImage();
+    }
+  }, [number, backgroundImageSrc, imageUrl]);
+
+  const handleDownload = () => {
+    if (canvasRef.current) {
+      canvasRef.current.toBlob((blob) => {
+        if (blob) {
+          const link = document.createElement("a");
+          link.href = URL.createObjectURL(blob);
+          link.download = `Custom-Monke-${number}.png`;
+          link.click();
+        }
+      }, "image/png");
+    }
+  };
 
   return (
     <div>
@@ -23,24 +78,29 @@ const MonkePngTool: React.FC = () => {
         type="text"
         value={number}
         onChange={handleInputChange}
-        placeholder="monke number"
+        placeholder="Monke number"
       />
-      {number && (
-        <>
-          <Image
-            width={500}
-            height={500}
-            src={imageUrl}
-            alt="User specified content"
-          />
-          <br />
-          <div className="flex justify-center mt-10">
-            <a href={imageUrl} download={`Image-${number}.png`}>
-              <Button>Download Image</Button>
-            </a>
-          </div>
-        </>
-      )}
+      <div className="mt-5 flex justify-center">
+        <p className="text-center"> Add a custom background</p>
+
+        <Input
+          type="file"
+          onChange={handleBackgroundImageChange}
+          title="Upload a background image"
+        />
+      </div>
+      <div className="flex justify-center">
+        <canvas
+          ref={canvasRef}
+          width="500"
+          height="500"
+          style={{ border: "1px solid black" }}
+        ></canvas>
+      </div>
+      <br />
+      {/* <div className="flex justify-center mt-10">
+        <Button onClick={handleDownload}>Download Combined Image</Button>
+      </div> */}
     </div>
   );
 };
