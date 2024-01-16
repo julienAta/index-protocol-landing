cb0a0b2b42c05f8136c1538aea84763c3f71199c
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-16 22:09:37 +0100

    style link

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -428,12 +428,15 @@ export function Landing() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">Made by jjx</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link
-            className="text-xs hover:underline underline-offset-4"
+          <a
             href="https://explorer.solana.com/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            only possible on solana
-          </Link>
+            <div className="text-xs text-gray-500 dark:text-gray-400">
+              only possible on solana
+            </div>
+          </a>
         </nav>
       </footer>
     </div>
