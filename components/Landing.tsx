ca27a2c15e4f97151a78c1aa1c9f839bcbfa4314
Atanassov <julienatanassov@delosintelligence.fr>
2024-01-16 20:03:23 +0100

    opos

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -427,14 +427,14 @@ export function Landing() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">Made by jjx</p>
-        {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-         
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link
+            className="text-xs hover:underline underline-offset-4"
+            href="https://explorer.solana.com/"
+          >
+            only possible on solana
           </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Privacy
-          </Link>
-        </nav> */}
+        </nav>
       </footer>
     </div>
   );
